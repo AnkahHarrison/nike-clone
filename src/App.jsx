c7be8aa11fcbb28c3312f19fed9aee1f73be5e87
Ankah <ankahharrison@gmail.com>
2024-04-24 00:12:35 +0000

    feat:added skeleton for various sections in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold text-center underline">
-      Site under maintenance
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section
+      className="xl:padding-1
+     wide:padding-r padding-b"
+    >
+      Hero
+    </section>
+    <section className="padding">PopularProducts</section>
+    <section className="padding">SuperQuality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">SpecialOffer</section>
+  </main>
+);
+
+export default App;
