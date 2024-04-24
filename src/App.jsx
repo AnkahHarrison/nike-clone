4a69c4499efc9dd91c812c2eed475633aae6f2d8
Ankah <ankahharrison@gmail.com>
2024-04-24 01:02:01 +0000

    feat:added starting file for various sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 const App = () => (
-  <main className="relative">
+  <main className="relative ">
     Nav
     <section
       className="xl:padding-1
@@ -11,6 +11,9 @@ const App = () => (
     <section className="padding">SuperQuality</section>
     <section className="padding-x py-10">Services</section>
     <section className="padding">SpecialOffer</section>
+    <section className="padding bg-pale-blue">CustomersReviews</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="padding-x padding-t -pb-8 bg-black">Footer</section>
   </main>
 );
 
---
+++ src/sections/CustomersReviews.jsx
@@ -0,0 +1,5 @@
+const CustomersReviews = () => {
+  return <div>CustomersReviews</div>;
+};
+
+export default CustomersReviews;
